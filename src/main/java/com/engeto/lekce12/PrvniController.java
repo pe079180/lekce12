21e3c42ac8b813c890ce2611151ed611847c0a8d
petr junker <petr.junker@o2.cz>
2022-05-30 19:52:10 +0200

    added getTeacherById

--- src/main/java/com/engeto/lekce12/PrvniController.java
+++ src/main/java/com/engeto/lekce12/PrvniController.java
@@ -1,8 +1,12 @@
 package com.engeto.lekce12;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class PrvniController {
@@ -12,7 +16,7 @@ public class PrvniController {
     }
 
     @GetMapping("/helloJson")
-    public Ucitel helloTeacher(){
+    public Ucitel helloTeacher() {
         Ucitel ucitel = new Ucitel();
         ucitel.setJmeno("Amos");
         ucitel.setPrijmeni("Komensky");
@@ -21,12 +25,31 @@ public class PrvniController {
     }
 
     @GetMapping("/helloParams")
-    public Ucitel helloTeacherWithParams(@RequestParam String jmeno, @RequestParam String prijmeni){
+    public Ucitel helloTeacherWithParams(@RequestParam String jmeno, @RequestParam String prijmeni) {
         Ucitel ucitel = new Ucitel();
         ucitel.setJmeno(jmeno);
         ucitel.setPrijmeni(prijmeni);
 
         return ucitel;
+    }
+
+    @GetMapping("/teachers/{id}")
+    public Ucitel getTeacherById(@PathVariable String id) {
+        //
+        Map<String, Ucitel> zakladDB = new HashMap<>();
+
+        Ucitel ucitel = new Ucitel();
+        ucitel.setJmeno("Amos");
+        ucitel.setPrijmeni("Komensky");
+
+        Ucitel ucitel2 = new Ucitel();
+        ucitel2.setJmeno("Jan");
+        ucitel2.setPrijmeni("Návrat");
+
+        zakladDB.put("1", ucitel);
+        zakladDB.put("2", ucitel2);
+
+        return zakladDB.get(id);
     }
 
 }
