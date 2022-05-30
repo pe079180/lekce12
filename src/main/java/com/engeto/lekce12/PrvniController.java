45c565ea00f5154eb603072639f4aba012f65556
petr junker <petr.junker@o2.cz>
2022-05-30 19:00:21 +0200

    Json added

--- src/main/java/com/engeto/lekce12/PrvniController.java
+++ src/main/java/com/engeto/lekce12/PrvniController.java
@@ -1,8 +1,21 @@
 package com.engeto.lekce12;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PrvniController {
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "Hello world ...";
+    }
 
+    @GetMapping("/helloJson")
+    public Ucitel helloTeacher(){
+        Ucitel ucitel = new Ucitel();
+        ucitel.setJmeno("Amos");
+        ucitel.setPrijmeni("Komensky");
+
+        return ucitel;
+    }
 }
---
+++ src/main/java/com/engeto/lekce12/Ucitel.java
@@ -0,0 +1,22 @@
+package com.engeto.lekce12;
+
+public class Ucitel {
+    private String jmeno;
+    private String prijmeni;
+
+    public String getJmeno() {
+        return jmeno;
+    }
+
+    public void setJmeno(String jmeno) {
+        this.jmeno = jmeno;
+    }
+
+    public String getPrijmeni() {
+        return prijmeni;
+    }
+
+    public void setPrijmeni(String prijmeni) {
+        this.prijmeni = prijmeni;
+    }
+}
