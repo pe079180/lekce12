10806a58955b2641270ca8d1845baf9b91ffb2e4
petr junker <petr.junker@o2.cz>
2022-05-30 19:20:12 +0200

    Params added

--- src/main/java/com/engeto/lekce12/PrvniController.java
+++ src/main/java/com/engeto/lekce12/PrvniController.java
@@ -1,6 +1,7 @@
 package com.engeto.lekce12;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,4 +19,14 @@ public class PrvniController {
 
         return ucitel;
     }
+
+    @GetMapping("/helloParams")
+    public Ucitel helloTeacherWithParams(@RequestParam String jmeno, @RequestParam String prijmeni){
+        Ucitel ucitel = new Ucitel();
+        ucitel.setJmeno(jmeno);
+        ucitel.setPrijmeni(prijmeni);
+
+        return ucitel;
+    }
+
 }
