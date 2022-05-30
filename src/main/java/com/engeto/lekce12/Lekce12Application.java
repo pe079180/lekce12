2be567d2f877027e9c895ed529aafb2fce2b3916
petr junker <petr.junker@o2.cz>
2022-05-30 17:14:13 +0200

    Initial commit

---
+++ src/main/java/com/engeto/lekce12/Lekce12Application.java
@@ -0,0 +1,11 @@
+package com.engeto.lekce12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lekce12Application {
+    public static void main(String[] args){
+        SpringApplication.run(Lekce12Application.class, args);
+    }
+}
---
+++ src/main/java/com/engeto/lekce12/PrvniController.java
@@ -0,0 +1,8 @@
+package com.engeto.lekce12;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PrvniController {
+
+}
